089ff145166d74b22200d997b857b3902cb1adda
momen mohamed <momenmohamed64@gmail.com>
2023-06-27 18:30:36 +0300

    added more max and min value for standerd types

--- math_calculator/StdTypes.h
+++ math_calculator/StdTypes.h
@@ -41,6 +41,11 @@ typedef enum {
 #define MAX_S8  ((s8)127)
 #define MIN_S8	((s8)-128)
 #define ZERO_S8 ((s8)0)
-
+#define MAX_S16 ((s16)32767)
+#define MIN_S16 ((s16)-32768)
+#define ZERO_S16 ((s16)0)
+#define MAX_S32 ((s32)2147483647)
+#define MIN_S32 ((s32)-2147483648)
+#define ZERO_S32 ((s32)0)
 
 #endif /* STDTYPES_H_ */
